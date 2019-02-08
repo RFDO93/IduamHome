c46f8075d28a7ba0e89c0b2098d0ba9527ec1249
Richard Felipe Dorta Oliveros <draox17@gmail.com>
2019-02-08 18:26:35 -0300

    Init

---
+++ index.php
@@ -0,0 +1,396 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iduam</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fullpage.min.css">
+</head>
+<body>
+    <div class="menu">
+        <div class="imagen">
+            <img src="image/Menu/LogoBlanco.svg" id="logo" alt="">
+        </div>
+        <div class="link">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">INICIO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">APLICACIÓN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SMARTBAND</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">INICIA SECION</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="fullpage">
+        <div class="home section" data-anchor="page0">
+            <div class="videoBackground" id="videoBackground"></div>
+        </div>
+        <div class="videos section" data-anchor="page1">
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <div class="row row60">
+                            <div class="col-md-12 img-video1 ">
+                                <div class="playVideo">
+                                    <i class="fas fa-play"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row40">
+                            <div class="col-6 img-video2">
+                                <div class="playVideo">
+                                    <i class="fas fa-play"></i>
+                                </div>
+                            </div>
+                            <div class="col-6 img-video3">
+                                <div class="playVideo">
+                                    <i class="fas fa-play"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 img-video4" >
+                        <div class="playVideo">
+                            <i class="fas fa-play"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec1 section" data-anchor="page2">
+            <div class="contenido "></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 subsec1">
+                        <img src="image/seccion1/Monitoreo.svg" class="imgText" alt="">
+                        <div class="titulo">
+                            <h1>MONITOREO 24-7</h1>
+                            <p>CALL CENTER MÉDICO</p>
+                        </div>
+                        <p>
+                            Cuando contratas Iduam y sus servicios, contratas la más especializada red
+                            de servicios de asistencia médica, existente en el país. Compuesta por un
+                            grupo interdisciplinario de especialistas en salud, telecomunicaciones y
+                            tecnología. Gracias al esfuerzo de todos y cada uno de ellos hemos
+                            conseguido crear un sistema que monitorea el estado de salud de nuestros
+                            pacientes de forma permamente y que brinda asistencia las 24 horas, de
+                            forma preventiva y reactiva frente a alteraciones físicas de nuestros
+                            pacientes, emergencias y consultas.
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-6 subsec1">
+                        <img src="image/seccion1/Monitoreos1@2x.png" alt="" class="imgSec1">
+                        <img src="image/seccion1/Monitoreos2@2x.png" alt="" class="imgSec1">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec2 section" data-anchor="page3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 subsec2">
+                        <img src="image/seccion2/Mediciones1@2x.png" alt="" class="imgSec2">
+                        <img src="image/seccion2/Mediciones2@2x.png" alt="" class="imgSec2">
+                    </div>
+                    <div class="col-12 col-md-6 subsec2">
+                        <img src="image/seccion2/IconoMediciones.svg" class="imgText" alt="">
+                        <div class="titulo">
+                            <h1>MEDICIONES</h1>
+                            <p>ALERTAS MANUALES</p>
+                        </div>
+                        <p>
+                            Mide de manera sencilla tus signos vitales con la Smatband Iduam Spark en
+                            conjuto con nuestra aplicación móvil iduam podras medir tu ritmo cardiaco,
+                            presión arterial y realizar tu propios electros cardiogramas usando.
+                            <br><br>
+                            Iduam te arrojara tus resultados de manera inmediata interpretados por un
+                            algoritmo de inteligencia artificial que te dirá si estas bien o tienes que
+                            consultar a tu medico.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec3 section" data-anchor="page4">
+            <div class="contenido"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 subsec3">
+                        <img src="image/seccion3/IconoAlertas.svg" class="imgText" alt="">
+                        <div class="titulo">
+                            <h1>ALERTAS</h1>
+                            <p>GEOREFENCIADAS</p>
+                        </div>
+                        <p>
+                            El envío de cada una de nuestras alertas y monitoreo en General, se realiza
+                            adjuntando toda la información que resulta relevante para nuestro cuerpo
+                            de asistencia.
+                            La hora, el día y el lugar son enviadas junto a un mapa en cada medición de
+                            nuestros pacientes, lo que permite que nuestros profesionales, frente a una
+                            emergencia o seguimiento de alteraciones en las mediciones, puedan
+                            tomar la mejor decición a la hora de asistir a nuestros pacientes.
+                            Cada una de las alertas enviadas se realiza enlazando la información que
+                            recopila la smartband y recepciona nuestro teléfono.
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-6 subsec3">
+                        <img src="image/seccion3/call-center.gif" class="ImagenGif" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec4 section" data-anchor="page5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 subsec4">
+                        <img src="image/seccion4/Notificaciones1@2x.png" alt="" class="imgSec4">
+                        <img src="image/seccion4/Notificaciones2@2x.png" alt="" class="imgSec4">
+                    </div>
+                    <div class="col-12 col-md-6 subsec4">
+                        <img src="image/seccion4/holter.gif" class="imgText" alt="">
+                        <div class="titulo">
+                            <h1>NOTIFICACIONES</h1>
+                            <p>ALERTAS</p>
+                        </div>
+                        <p>
+                            Para mayor presición estadistico en los niveles de presión arterial, saturación
+                            de oxigeno y ritmo cardiaco.<br><br>
+                            Holter es una de las herramientas que conforman la aplicación de Iduam
+                            que le permite al usuario, programar varias mediciones en un mismo día,
+                            manteniendo su información biológica constante y previniendo de forma
+                            más segura un acontesimiento en hipertensión.<br><br>
+                            A demás, mantiene su histrial medico actualizado brindando soporte
+                            inmediato online, con medicos profesionales
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec5 section" data-anchor="page6">
+            <div class="contenido"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6 subsec5">
+                        <img src="image/seccion5/IconoIduam.svg" class="imgText" alt="">
+                        <div class="titulo">
+                            <h1>IDUAM</h1>
+                            <p>PA R A T U COMUNA</p>
+                        </div>
+                        <p>
+                            “IDUAM”, es la Salud al alcance de TODOS y a un sólo CLIC.
+                            <br><br>
+                            Brindandole a tu comunidad un servicio unico que entrega distintos canales
+                            de comunicación total, entre el médico y pacientes postrados o con
+                            movilidad reducida, o bien, con pacientes potenciales y a nivel masivo.
+                            <br><br>
+                            Permite a los Doctores tener la posibilidad de ampliar el número de
+                            pacientes a los que pueden atender diariamente, extendiendo el rango
+                            geográfico de acción en sus Atenciones médicas, en donde podrá realizar
+                            mayores controles médicos preventivos, reduciendo o eliminando los
+                            tiempos de viaje, todo lo cual incrementa la frecuencia de controles
+                            médicos entre muchos otros beneficios.
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-6 subsec5">
+                        <img src="image/seccion5/comuna.gif" class="imgSec5" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sec6 section" data-anchor="page7">
+            <div class="subSec1">
+                <div class="container">
+                    <h1>MANTEN UNA VIDA CON MENOS RIESGOS</h1>
+                    <div class="lista">
+                        <ol>
+                            <li>Reciba información diaria de salud.</li>
+                            <li>Recetarios de alimentación.</li>
+                            <li>Recomendaciones medicas.</li>
+                        </ol>
+                    </div>
+                    <div class="div">
+                        <input type="text" placeholder="E-mail">
+                        <button type="button">Enviar</button>
+                    </div>
+                    <button type="button">Descargar</button>
+                </div>
+            </div>
+            <div class="subSec2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 linkSubSec2">
+                            <a href="#">PREGUNTAS FRECUENTES</a>
+                            <a href="#">IDUAM</a>
+                            <a href="#">SMARTBAND</a>
+                            <a href="#">CONTACTO</a>
+                            <a href="#">APLICACIÓN</a>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="LogoFooter">
+                                <img src="image/Menu/LogoBlanco.svg" alt="">
+                                <p>Obtener versión 2.0</p>
+                            </div>
+                            <div class="DescargasFooter">
+                                <img src="image/footer/googlePlay.png" alt="">
+                                <img src="image/footer/appStore.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-4 letterSubSec2 textRightSubSec2">
+                            <hr style="width: 80%;background-color: white">
+                            <p>NEWSLETTERS</p>
+                            <div class="inputTextRightSubSec2">
+                                <input type="text" placeholder="E-mail">
+                                <button type="button">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="subSec3">
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-google-plus-g"></i>
+                <i class="fab fa-google-play"></i>
+            </div>
+            <div class="subSec4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col d-flex justify-content-start align-items-center">
+                            <p>Copyright © 2019 | All rights reserved.</p>
+                        </div>
+                        <div class="col d-flex justify-content-end align-items-center">
+                            <p>Mapa del sitio</p>
+                            <img src="image/footer/chile.png" alt="">
+                            <p>chile</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scrolloverflow.min.js"></script>
+<script src="js/jquery.fullPage.js"></script>
+<script src="js/fontawesome-all.js"></script>
+<script src="js/vimeo-player.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            verticalCentered: false,
+            css3: true,
+            navigation: true,
+            navigationPosition: 'right',
+
+            onLeave: function (index, nextIndex, direction) {
+
+                if(nextIndex == 1){
+                    $("#logo").attr('src','image/Menu/LogoBlanco.svg');
+                    $(".nav-link").css("color","#ffffff");
+                    $(".menu").removeClass('menuSinFondo');
+                    playVideo();
+                } else if (nextIndex == 3){
+                    setTimeout(function () {
+                        $("#logo").attr('src','image/Menu/LogoNegro.svg');
+                        $(".nav-link").css("color","#000000");
+                        $(".menu").addClass("menuSinFondo");
+                        playVideo(false);
+                    },500);
+                }else{
+                    setTimeout(function () {
+                        $("#logo").attr('src','image/Menu/LogoBlanco.svg');
+                        $(".nav-link").css("color","#ffffff");
+                        $(".menu").addClass("menuSinFondo");
+                        playVideo(false);
+                    },500);
+                }
+
+                if (nextIndex == 7 || nextIndex == 8) {
+                    $(".menu").css("visibility","hidden");
+                }else{
+                    $(".menu").css("visibility","visible");
+                }
+
+
+            },
+        });
+
+        //methods
+        $.fn.fullpage.setAllowScrolling(true);
+
+        let options = {
+            id : 316159723,
+            autoplay:true,
+            loop: true
+        };
+
+        let player = new Vimeo.Player('videoBackground',options);
+        let isPause = false;
+        player.on('play', function () {
+            isPause = true;
+        });
+
+        function playVideo(aux = true) {
+            if (aux){
+                player.play().then(function() {
+                    // the video was played
+                }).catch(function(error) {
+                    switch (error.name) {
+                        case 'PasswordError':
+                            // the video is password-protected and the viewer needs to enter the
+                            // password first
+                            break;
+
+                        case 'PrivacyError':
+                            // the video is private
+                            break;
+
+                        default:
+                            // some other error occurred
+                            break;
+                    }
+                });
+            } else {
+                if (isPause){
+                    player.pause().then(function() {
+                        console.log('pause');
+                        isPause = false;
+                    }).catch(function(error) {
+                        switch (error.name) {
+                            case 'PasswordError':
+                                // the video is password-protected and the viewer needs to enter the
+                                // password first
+                                break;
+
+                            case 'PrivacyError':
+                                // the video is private
+                                break;
+
+                            default:
+                                // some other error occurred
+                                break;
+                        }
+                    });
+                }
+
+            }
+
+        }
+
+    });
+</script>
+</body>
+</html>
